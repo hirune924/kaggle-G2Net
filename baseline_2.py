1845bc086441d50c292b7925b0d817111f115381
hirune924 <36130517+hirune924@users.noreply.github.com>
2021-08-23 08:37:56 +0900

    Update baseline_2.py

--- baseline_2.py
+++ baseline_2.py
@@ -188,7 +188,8 @@ class SETIDataModule(pl.LightningDataModule):
             #            #A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225), max_pixel_value=255.0, always_apply=False, p=1.0)
             #            ])
 
-            self.train_dataset = G2NetDataset(train_df, transform=train_transform,conf=self.conf)
+            #self.train_dataset = G2NetDataset(train_df, transform=train_transform,conf=self.conf)
+            self.train_dataset = G2NetDataset(train_df, transform=None,conf=self.conf)
             self.valid_dataset = G2NetDataset(valid_df, transform=None, conf=self.conf)
             
         #elif stage == 'test':
