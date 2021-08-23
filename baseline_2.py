f17b8b1e0d374dca3cb74ca745abd1f1d5609ece
hirune924 <36130517+hirune924@users.noreply.github.com>
2021-08-23 13:16:41 +0900

    add normalize

--- baseline_2.py
+++ baseline_2.py
@@ -105,6 +105,8 @@ class G2NetDataset(Dataset):
         image = self.apply_qtransform(waves, self.wave_transform)
         image = image.squeeze().numpy().transpose(1,2,0)
 
+        image = (image-np.mean(image, axis=(0,1),keepdims=True))/np.std(image, axis=(0,1),keepdims=True)
+
         #img_pl = Image.fromarray(image).resize((self.conf.height, self.conf.width), resample=Image.BICUBIC)
         #image = np.array(img_pl)
         #image = cv2.resize(image, (self.conf.width, self.conf.height), interpolation=cv2.INTER_CUBIC)
