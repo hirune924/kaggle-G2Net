0c4bcc7ed633b99a083068bc57bd2d6c05c866d9
hirune924 <36130517+hirune924@users.noreply.github.com>
2021-09-15 07:58:34 +0900

    add cnn1d org

---
+++ model/cnn1dorg.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+
+class CNN1d(nn.Module):
+    def __init__(self):
+        super(CNN1d, self).__init__()
+        self.cnn = nn.Sequential(
+            nn.Conv1d(3, 64,kernel_size=64, stride=2, padding=32),
+            nn.BatchNorm1d(64),nn.ReLU(),nn.MaxPool1d(kernel_size=2, stride=1),
+            nn.Conv1d(64, 128,kernel_size=64, stride=2, padding=32),
+            nn.BatchNorm1d(128),nn.ReLU(),nn.MaxPool1d(kernel_size=2, stride=1),
+            nn.Conv1d(128, 256,kernel_size=64, stride=2, padding=32),
+            nn.BatchNorm1d(256),nn.ReLU(),nn.MaxPool1d(kernel_size=2, stride=1),
+            nn.Conv1d(256, 256,kernel_size=64, stride=2, padding=32),
+            nn.BatchNorm1d(256),nn.ReLU(),nn.MaxPool1d(kernel_size=2, stride=1))
+
+        self.fc = nn.Seuquential(nn.AdaptiveAvgPool1d(1),nn.Flatten,nn.Linear(256,1))
+    def forward(self, x):
+        x = self.cnn(x)
+        x = self.fc(x)
+        return x
+        
