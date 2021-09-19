a28fb7a8b16d0ad910e9673a192beb9562e19fca
hirune924 <36130517+hirune924@users.noreply.github.com>
2021-09-20 06:40:44 +0900

    bugfix

--- baseline_3_fixnorm_randwin_resize_ousm.py
+++ baseline_3_fixnorm_randwin_resize_ousm.py
@@ -314,8 +314,8 @@ class LitSystem(pl.LightningModule):
 
         if len(losses.shape) == 2:
             losses = losses.mean(1)
-        print(losses.shape)
-        print(int(self.hparams.batch_size * 0.6))
+        #print(losses.shape)
+        #print(int(self.hparams.batch_size * 0.6))
         _, idxs = losses.topk(int(self.hparams.batch_size * 0.6), largest=False)
         losses = losses.index_select(0, idxs)
         
